3eb9d8ff87effcd69685a32e6ecfa41631356532
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-12-24 19:27:03 +0530

    AppNav

---
+++ src/Components/AppNav/AppNav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const AppNav = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>AppNav</h1>
+      </div>
+    </>
+  );
+};
+
+export default AppNav;
