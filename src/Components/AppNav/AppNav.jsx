c075b2a345aca7efeffd62fd1bb6e93ca4d9fa96
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-12-24 19:57:30 +0530

    first design

--- src/Components/AppNav/AppNav.jsx
+++ src/Components/AppNav/AppNav.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
 
-const AppNav = () => {
+const AppNav = (props) => {
   return (
     <>
-      <Navbar />
-      <div>
-        <h1>AppNav</h1>
+      <div className="nav d-flex justify-content-between p-3 bg-warning align-items-center">
+        <div className="nav-items">
+          <h1>AppNav</h1>
+        </div>
+        <div className="nav-items">
+          <ul className="row list-unstyled m-0">
+            <li className="nav-links col">Home</li>
+            <li className="nav-links col">About</li>
+            <li className="nav-links col">Contact</li>
+          </ul>
+        </div>
       </div>
     </>
   );
