7dc2bfc27e968d92c30f1c71b027ababd65e0199
Akshat Singh <singh.akshat.akshat26@gmail.com>
2021-12-30 18:51:49 +0530

    navbar

--- src/Components/AppNav/AppNav.jsx
+++ src/Components/AppNav/AppNav.jsx
@@ -7,31 +7,26 @@ const AppNav = (props) => {
 
   return (
     <>
-      <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">
-            <Image src={logo} width="100" height="100" className="d-inline-block align-top" alt="logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
+    
+         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+         <div className="container"  >
+                <Navbar.Brand href="#home">
+                  <img src={logo} width="70" height="70" alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link href="#deets">Contact</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                          
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </div>
+            </Navbar>
+           
     </>
   );
 };
