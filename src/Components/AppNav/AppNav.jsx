63bb1010bec47bef27723b3493f1eb753400ffe9
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-12-30 22:55:33 +0530

    fix

--- src/Components/AppNav/AppNav.jsx
+++ src/Components/AppNav/AppNav.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Image,
-  Container,
-} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Image, Container } from "react-bootstrap";
 import logo from "../../Assets/images/logo/logo.png";
 import AppButton from "../AppButton/AppButton";
 import navcss from "./AppNav.module.css";
@@ -26,17 +20,7 @@ const AppNav = (props) => {
             <Nav>
               <Nav.Link href="#features">Home</Nav.Link>
               <Nav.Link href="#pricing">About</Nav.Link>
-<<<<<<< HEAD
               <NavDropdown title="Services" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-=======
-              <NavDropdown title="Service" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">
                   Another action
@@ -46,18 +30,19 @@ const AppNav = (props) => {
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
->>>>>>> 42c5e243ba868fecfe47f374230a88c54cf26759
               <Nav.Link href="#deets">Contact</Nav.Link>
               <Nav.Link eventKey={2} href="#memes"></Nav.Link>
-              <Nav.Link >
-                <AppButton label={"Login"} withBg className={"border-0 rounded-pill"} />
+              <Nav.Link>
+                <AppButton
+                  label={"Login"}
+                  withBg
+                  className={"border-0 rounded-pill"}
+                />
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
-        
       </Navbar>
-      
     </>
   );
 };
