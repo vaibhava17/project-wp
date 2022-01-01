3778ae7a917a378db8c97b4ebe8efd315ea452d5
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-01-01 13:55:27 +0530

    footer

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -6,26 +6,39 @@ const AppFooter = (props) => {
   return (
     <>
       <div className="footer">
-        
         <div className="container py-5">
           <div className="row">
             <div className="col-md-3 col-sm-6 ">
-              <div className="row justify-content-center">
-              <div className="footer-logo col-12">
-                <img src={logo} width="50" height="50" alt="logo" />
-                <h6>Webpaage <span>Technologies</span></h6>
-
-              </div>
-              <hr />
-              <div className="col-12 ">
-                <ul className="list-unstyled list-group list-group-horizontal mx-auto">
-                <li className="list-group-item"><i class="fab fa-instagram"></i></li>
-                <li className="list-group-item"><i class="fab fa-facebook"></i></li>
-                <li className="list-group-item"><i class="fab fa-github"></i></li>
-                <li className="list-group-item"><i class="fab fa-linkedin-in"></i></li>
-                <li className="list-group-item"><i class="fab fa-twitter"></i></li>
-                </ul> </div>
+              <div className="row">
+                <div className="footer-logo text-center col">
+                  <img src={logo} width="50" height="50" alt="logo" />
+                  <h6>
+                    Webpaage <span>Technologies</span>
+                  </h6>
                 </div>
+                <div className="w-100">
+                  <hr />
+                </div>
+                <div className="col">
+                  <ul className="list-unstyled justify-content-center list-group list-group-horizontal mx-auto">
+                    <li className="list-group-item">
+                      <i class="fab fa-instagram"></i>
+                    </li>
+                    <li className="list-group-item">
+                      <i class="fab fa-facebook"></i>
+                    </li>
+                    <li className="list-group-item">
+                      <i class="fab fa-github"></i>
+                    </li>
+                    <li className="list-group-item">
+                      <i class="fab fa-linkedin-in"></i>
+                    </li>
+                    <li className="list-group-item">
+                      <i class="fab fa-twitter"></i>
+                    </li>
+                  </ul>{" "}
+                </div>
+              </div>
             </div>
             <div className="col-md-3 col-sm-6 ">
               <div className="footer-links">
@@ -51,7 +64,7 @@ const AppFooter = (props) => {
                 <h4>About</h4>
                 <ul className="list-unstyled ">
                   <li className="text-decoration-none">
-                    <a href="#" >About Us</a>
+                    <a href="#">About Us</a>
                   </li>
                   <li>
                     <a href="#">Our Team</a>
