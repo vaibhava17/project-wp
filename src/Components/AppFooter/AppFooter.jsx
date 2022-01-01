e0f1b1a1d1680b9295014947b20dc4292679999f
Akshat Singh <singh.akshat.akshat26@gmail.com>
2022-01-01 13:29:00 +0530

    footer

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -6,18 +6,19 @@ const AppFooter = (props) => {
   return (
     <>
       <div className="footer">
-        <div className="container">
+        
+        <div className="container py-5">
           <div className="row">
-            <div className="col">
+            <div className="col-md-3 col-sm-6">
               <div className="footer-logo">
                 <img src={logo} width="100" height="100" alt="logo" />
                 <h4>Webpaage Technologies</h4>
               </div>
             </div>
-            <div className="col">
+            <div className="col-md-3 col-sm-6 ">
               <div className="footer-links">
                 <h4>Services</h4>
-                <ul>
+                <ul className="list-unstyled">
                   <li>
                     <a href="#">Web Development</a>
                   </li>
@@ -33,10 +34,29 @@ const AppFooter = (props) => {
                 </ul>
               </div>
             </div>
-            <div className="col">
+            <div className="col-md-3 col-sm-6">
               <div className="footer-links">
                 <h4>About</h4>
-                <ul>
+                <ul className="list-unstyled ">
+                  <li className="text-decoration-none">
+                    <a href="#" >About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Our Team</a>
+                  </li>
+                  <li>
+                    <a href="#">Our Partners</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3 col-sm-6 ">
+              <div className="footer-links">
+                <h4>About</h4>
+                <ul className="list-unstyled">
                   <li>
                     <a href="#">About Us</a>
                   </li>
