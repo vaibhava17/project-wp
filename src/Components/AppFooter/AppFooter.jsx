8633a08f4a6cb3c966aeb5f604fad9a412556c79
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-01-01 13:03:00 +0530

    footer

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -5,25 +5,68 @@ import logo from "../../Assets/images/logo/logo.png";
 const AppFooter = (props) => {
   return (
     <>
-  <div className="footer">
-    <div className="container">
-      <div className="row">
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="footer-logo">
+                <img src={logo} width="100" height="100" alt="logo" />
+                <h4>Webpaage Technologies</h4>
+              </div>
+            </div>
+            <div className="col">
+              <div className="footer-links">
+                <h4>Services</h4>
+                <ul>
+                  <li>
+                    <a href="#">Web Development</a>
+                  </li>
+                  <li>
+                    <a href="#">Web Design</a>
+                  </li>
+                  <li>
+                    <a href="#">Web Hosting</a>
+                  </li>
+                  <li>
+                    <a href="#">Web Maintenance</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col">
+              <div className="footer-links">
+                <h4>About</h4>
+                <ul>
+                  <li>
+                    <a href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Our Team</a>
+                  </li>
+                  <li>
+                    <a href="#">Our Partners</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/*         
         <div className="col mt-5 text-center">
-        <img src={logo} width="100" height="100" alt="logo" />
         <br />
-        <h4>Webpaage Technologies</h4>
         </div>
-        <div className="col dflex align-self-center">
+        <div className="col d-flex">
           <a href="#">Home</a>
           <a href="#">Home</a>
           <a href="#">Home</a>
           <a href="#">Home</a>
         </div>
         <div className="col"></div>
-        <div className="col"></div>
+        <div className="col"></div> */}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     </>
   );
 };
