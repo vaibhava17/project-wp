484a6bbb7b3858acaa5d4d34374abbbb4cfe32ee
Akshat Singh <singh.akshat.akshat26@gmail.com>
2022-01-02 12:34:19 +0530

    carousel

--- src/Components/Carsoule/Carsoule.jsx
+++ src/Components/Carsoule/Carsoule.jsx
@@ -2,27 +2,51 @@ import { Carousel } from 'antd';
 
 
 const contentStyle = {
-  height: '160px',
+  height: '',
+
   color: '#fff',
-  lineHeight: '160px',
+  
   textAlign: 'center',
-  background: '#364d79',
+ 
+  background: '#046da9',
+  
+  
+  
+
 };
 
-const AppCarousel=()=> {
+const AppCarousel=({src})=> {
     return (
-  <Carousel autoplay>
+  <Carousel >
     <div>
-      <h3 style={contentStyle}>1</h3>
+    <div style={contentStyle}>
+      <div className='container'>
+      <div className='row p-5'>
+        <div className='col-sm carousel '>
+          <br></br>
+         <h3>Grow Your Business Online With Us</h3>
+         <br></br>
+         <p >When everything is online then why are'nt you? Take your buisness online and reach the people all over the world not just in Bareilly or UP. Create your professional looking website design today. Or already have a website looking to take it one step ahead? We will develop your website with the look you imagined.
+Just Contact Us!! Support local developers from Bareilly.
+Take a tour of our website and browse through amazing web development plans today!</p>
+        </div>
+        
+        <div className='col-sm'>
+        <br></br>
+       <img src={src} width="80%" height="80%" alt="logo" />
+        </div>
+      </div>
+      </div>
     </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
     </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
+    <div >
+    <div style={contentStyle}>ksndkln</div>
     </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
+    <div >
+    <div style={contentStyle}></div>
+    </div>
+    <div >
+    <div style={contentStyle}></div>
     </div>
   </Carousel>
     )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppCarousel from "../Components/Carsoule/Carsoule";
+import img from "../Assets/images/carousel/c6.png";
 
 
 const Home = (props) => {
   return (
     <>
-   <AppCarousel />
+   <AppCarousel src={img} />
+   
     </>
   );
 };
