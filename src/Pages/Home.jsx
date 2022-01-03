d4767713fc065dee8920a71b453ec045d21a7e9f
Akshat Singh <singh.akshat.akshat26@gmail.com>
2022-01-03 15:09:57 +0530

    carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppCarousel from "../Components/AppCarousel/Carousel";
 import img from "../Assets/images/carousel/c6.png";
+import img2 from "../Assets/images/carousel/c7.png";
+import img3 from "../Assets/images/carousel/seo.png";
+import img4 from "../Assets/images/carousel/graphics.png";
 
 const Home = (props) => {
   return (
     <>
-      <AppCarousel src={img} />
+      <AppCarousel src={img} sr={img2} seo={img3} graphics={img4} />
     </>
   );
 };
